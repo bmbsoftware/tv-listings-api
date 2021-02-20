36e2b4c01b2dd10384a9eb269a1e7dbab1848245
Brian Baker <bbaker@bmbsoftwaresolutions.com>
2021-02-20 08:46:40 -0600

    Updated listings and packages

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var xml = require('xml');
-var fs = require('fs');
-var moment = require('moment');
+const express = require('express');
+const bodyParser = require('body-parser');
+const xml = require('xml');
+const fs = require('fs');
+const moment = require('moment');
 
-var app = express();
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3001;
-var router = express.Router();
+const port = process.env.PORT || 3001;
+const router = express.Router();
 
 function getListings(days) {
 	console.log('getting program data for ' + days + ' days...');
-	var programSourceData = JSON.parse(fs.readFileSync('./data/tvListings.json', 'utf8'));
+	const programSourceData = JSON.parse(fs.readFileSync('./data/tvListings.json', 'utf8'));
 
-	var data = JSON.parse(JSON.stringify(programSourceData));
+	const data = JSON.parse(JSON.stringify(programSourceData));
 	data.listings[0].tv = [];
 
-	var programCount = 0;
+	let programCount = 0;
 	programSourceData.listings[0].tv.forEach(function (element) {
 		if (element.programme) {
-			var format = 'YYYYMMDDHHmmss ZZ';
-			for (var day = 0; day < days; day++) {
-				var today = moment().startOf("day").add(day, "day").format(format);
-				var tomorrow = moment().startOf("day").add(day + 1, "day").format(format);
+			const format = 'YYYYMMDDHHmmss ZZ';
+			for (let day = 0; day < days; day++) {
+				const today = moment().startOf("day").add(day, "day").format(format);
+				const tomorrow = moment().startOf("day").add(day + 1, "day").format(format);
 				element.programme[0]._attr.start = today;
 				element.programme[0]._attr.stop = tomorrow;
 				data.listings[0].tv.push(JSON.parse(JSON.stringify(element)));
@@ -58,11 +58,16 @@ router.get('/xml', function (req, res) {
 });
 
 router.get('/xml/tvlistings/:days', function (req, res) {
-	var data = xml(getListings(req.params.days || 1).listings, { indent: '  ', declaration: true });
+	const data = xml(getListings(req.params.days || 1).listings, { indent: '  ', declaration: true });
 	res.set('Content-Type', 'text/xml');
 	res.send(data);
 });
 
+router.get('/xml/guide', async function (req, res) {
+	const xmltv = fs.readFileSync('./data/tvlistings.xmltv', 'utf8');
+	res.set('Content-Type', 'text/xml');
+	res.send(xmltv);
+});
 
 app.use('/api', router);
 
